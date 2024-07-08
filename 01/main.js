ace440d6bf65666f3a3e8fa73ae718bb9337ae1d
Ryszard Błażej <sapermysza@gmail.com>
2024-07-08 13:45:27 +0200

    Tidying up

--- 01/main.js
+++ 01/main.js
@@ -1,33 +1,39 @@
 import * as THREE from 'three';
 
+// basic declarations
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+// cube instance
+const geometry = new THREE.BoxGeometry(2, 2, 2);
 
+// adding texture to the cube
 const texture = new THREE.TextureLoader().load("/textures/fish.jpg")
 
 const material = new THREE.MeshBasicMaterial({
     map: texture
 });
 
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
+// light and camera settings
 const pointLight = new THREE.PointLight(0xFFFFFF)
-scene.add( pointLight )
+scene.add(pointLight)
 
 scene.background = new THREE.Color(0x7788ff)
 
 camera.position.z = 5;
 
+
 function animate() {
+    // rotatiion animation
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
-renderer.setAnimationLoop( animate );
+renderer.setAnimationLoop(animate);
--- 02/main.js
+++ 02/main.js
@@ -1,45 +1,51 @@
 import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
+
+// basic declarations
 var width = 100;
 var height = 56;
 
 const scene = new THREE.Scene();
-const camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 0.1, 1000 );
+const camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 
+// loading .fbx model
 const loader = new FBXLoader();
 
-loader.load( '/models/Koi_Tri.fbx', function ( object ) {
-    object.rotateY(Math.PI/2)
+loader.load('/models/Koi_Tri.fbx', function (object) {
+    object.rotateY(Math.PI / 2)
     object.translateX(10)
     object.scale.set(.015, .015, .015)
     object.name = "koi_fish"
-	scene.add( object );
-}, undefined, function ( error ) {
-	console.error( error );
-} );
+    scene.add(object);
+}, undefined, function (error) {
+    console.error(error);
+});
 
+// light and camera settings
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.7)
-scene.add( ambientLight )
+scene.add(ambientLight)
 
 scene.background = new THREE.Color(0x7788ff)
 
 camera.position.z = 5;
 
+// mouse position capture
 var mouse = new THREE.Vector3()
 
-function OnMouseMove(e){
-	mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+function OnMouseMove(e) {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
     mouse.z = -1;
     mouse.unproject(camera);
 }
 
+// animation parameters
 var current_rotation = 0;
 
 var fish_rot_speed = 0.03;
@@ -60,41 +66,43 @@ function animate() {
 
     const koi_fish = scene.getObjectByName("koi_fish");
 
-    if(koi_fish) {
+    if (koi_fish) {
         var target_angle = Math.atan2(mouse.y - koi_fish.position.y, mouse.x - koi_fish.position.x);
         var diff_rot = current_rotation - target_angle;
-        
-        console.log(current_rotation)
 
-        while(diff_rot < -Math.PI){
+        // clamping rotation in <-pi, pi>
+        while (diff_rot < -Math.PI) {
             diff_rot += 2 * Math.PI;
         }
-        while(diff_rot > Math.PI){
+        while (diff_rot > Math.PI) {
             diff_rot -= 2 * Math.PI;
         }
 
-        if(current_y_steps == 0 && flip && (target_angle < -Math.PI / 2 || target_angle > Math.PI / 2)) {
+        // conditions for y-axis turn
+        if (current_y_steps == 0 && flip && (target_angle < -Math.PI / 2 || target_angle > Math.PI / 2)) {
             current_y_steps = y_steps;
             flip = false;
         }
-        else if(current_y_steps == 0 && !flip && (target_angle >= -Math.PI / 2 && target_angle <= Math.PI / 2)) {
+        else if (current_y_steps == 0 && !flip && (target_angle >= -Math.PI / 2 && target_angle <= Math.PI / 2)) {
             current_y_steps = y_steps;
             flip = true;
         }
 
-        if(current_y_steps != 0){
+        // animation of x-axis turn
+        if (current_y_steps != 0) {
             current_y_steps -= 1;
-            if(flip){
+            if (flip) {
                 koi_fish.rotateY(Math.PI / y_steps);
             }
-            else{
+            else {
                 koi_fish.rotateY(-Math.PI / y_steps);
             }
         }
-        else{
-            if(diff_rot > 0){
-                if(direction){
-                    if(fish_current_rot_acc + fish_step_rot_acc < fish_max_rot_acc){
+        else {
+            if (diff_rot > 0) {
+                // rotation acceleration
+                if (direction) {
+                    if (fish_current_rot_acc + fish_step_rot_acc < fish_max_rot_acc) {
                         fish_current_rot_acc += fish_step_rot_acc;
                     }
                     else {
@@ -104,16 +112,19 @@ function animate() {
                 else {
                     fish_current_rot_acc = 0.0;
                 }
+
+                // x-axis rotation
                 koi_fish.rotateX(fish_rot_speed + fish_current_rot_acc);
-                
-                if(flip) current_rotation -= (fish_rot_speed + fish_current_rot_acc);
+
+                if (flip) current_rotation -= (fish_rot_speed + fish_current_rot_acc);
                 else current_rotation += (fish_rot_speed + fish_current_rot_acc);
-        
+
                 direction = true;
             }
             else {
-                if(!direction){
-                    if(fish_current_rot_acc + fish_step_rot_acc < fish_max_rot_acc){
+                //rotation acceleration
+                if (!direction) {
+                    if (fish_current_rot_acc + fish_step_rot_acc < fish_max_rot_acc) {
                         fish_current_rot_acc += fish_step_rot_acc;
                     }
                     else {
@@ -123,18 +134,24 @@ function animate() {
                 else {
                     fish_current_rot_acc = 0.0;
                 }
+
+                // x-axis rotation
                 koi_fish.rotateX(-(fish_rot_speed + fish_current_rot_acc));
-                if(flip) current_rotation += (fish_rot_speed + fish_current_rot_acc);
+
+                if (flip) current_rotation += (fish_rot_speed + fish_current_rot_acc);
                 else current_rotation -= (fish_rot_speed + fish_current_rot_acc);
+
                 direction = false;
             }
 
+            // movement forward
             koi_fish.translateZ(fish_move_speed + koi_fish.position.distanceTo(mouse) * fish_magnitude_speed_factor);
         }
     }
 
-	renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
 
-window.addEventListener( 'mousemove', OnMouseMove, false );
-renderer.setAnimationLoop( animate );
+// adding listener to movement of mouse
+window.addEventListener('mousemove', OnMouseMove, false);
+renderer.setAnimationLoop(animate);
